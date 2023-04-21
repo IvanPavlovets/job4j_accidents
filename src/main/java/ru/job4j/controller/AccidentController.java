f2dcaee948c5906945accf0c7ae437c345415f5e
IvanPavlovets <ivan150287@gmail.com>
2023-04-21 13:27:08 +0700

    3.1. @RequestParam. Форма редактирования [#308708]

--- src/main/java/ru/job4j/controller/AccidentController.java
+++ src/main/java/ru/job4j/controller/AccidentController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.job4j.model.Accident;
 import ru.job4j.service.AccidentService;
 
@@ -15,6 +16,11 @@ public class AccidentController {
 
     private final AccidentService service;
 
+    /**
+     * Обрабатывает переход на createAccident.html
+     * @param model
+     * @return String
+     */
     @GetMapping("/createAccident")
     public String viewCreateAccident(Model model) {
         model.addAttribute("user", "Ivan Pavlovets");
@@ -22,9 +28,41 @@ public class AccidentController {
         return "createAccident";
     }
 
+    /**
+     * Обрабатывает добавление данных в accident
+     * и их сохранение в store.
+     * @param accident
+     * @return
+     */
     @PostMapping("/saveAccident")
     public String save(@ModelAttribute Accident accident) {
         service.add(accident);
         return "redirect:/index";
     }
+
+    /**
+     * Обработывает переход на editAccident.html
+     * @param id
+     * @param model
+     * @return String
+     */
+    @GetMapping("/formEditAccident")
+    public String viewEditAccident(@RequestParam("id") int id, Model model) {
+        model.addAttribute("user", "Ivan Pavlovets");
+        model.addAttribute("editableAccident", service.findById(id));
+        return "editAccident";
+    }
+
+    /**
+     * Сохраняет данные в accident
+     * после редактирования
+     * @param accident
+     * @return String
+     */
+    @PostMapping("/editAccident")
+    public String update(@ModelAttribute Accident accident) {
+        service.update(accident);
+        return "redirect:/index";
+    }
+
 }
