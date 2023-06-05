407d2ee2c61f0bd43bbe5dbbf394ee6404ced309
IvanPavlovets <ivan150287@gmail.com>
2023-06-05 18:58:58 +0700

    1. Spring mock [#2096 # [#2096]]

--- src/test/java/ru/job4j/controller/AccidentControllerTest.java
+++ src/test/java/ru/job4j/controller/AccidentControllerTest.java
@@ -1,13 +1,17 @@
 package ru.job4j.controller;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
 
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,11 +20,14 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.Main;
 import ru.job4j.model.Accident;
+import ru.job4j.model.AccidentType;
 import ru.job4j.service.springdata.AccidentSpringDataService;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
 import java.util.Optional;
+import java.util.Set;
+
 @Transactional
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
@@ -30,6 +37,82 @@ public class AccidentControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    /**
+     * Проверка метода save из AccidentController
+     * @throws Exception
+     */
+    @Test
+    @WithMockUser
+    void whenSaveAccidentThenRedirectIndexPage() throws Exception {
+        var rIds = Set.of(1, 2, 3);
+        var accident = new Accident(0, "11", "22", "33",
+                new AccidentType(2, "Машина и человек"), Collections.emptySet());
+        this.mockMvc.perform(post("/saveAccident")
+                        .param("name", accident.getName())
+                        .param("text", accident.getText())
+                        .param("address", accident.getAddress())
+                        .param("type", "2")
+                        .param("rIds", "1", "2", "3"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/index"));
+
+        ArgumentCaptor<Accident> accidentCapture = ArgumentCaptor.forClass(Accident.class);
+        ArgumentCaptor<Set<Integer>> rIdsCapture = ArgumentCaptor.forClass(Set.class);
+
+        verify(accidents).add(accidentCapture.capture(), rIdsCapture.capture());
+
+        assertThat(accidentCapture.getValue()).usingRecursiveComparison()
+                .isEqualTo(accident);
+        assertThat(rIdsCapture.getValue()).usingRecursiveComparison()
+                .isEqualTo(rIds);
+    }
+
+    /**
+     * Проверка метода update из AccidentController
+     * @throws Exception
+     */
+    @Test
+    @WithMockUser
+    void whenEditAccidentThenRedirectIndexPage() throws Exception {
+        var rIds = Set.of(1, 2, 3);
+        var accident = new Accident(0, "11", "22", "33",
+                new AccidentType(2, "Машина и человек"), Collections.emptySet());
+        this.mockMvc.perform(post("/editAccident")
+                        .param("id", "0")
+                        .param("name", "11")
+                        .param("text", "22")
+                        .param("address", "33")
+                        .param("type", "2")
+                        .param("rIds", "1", "2", "3"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/index"));
+
+        ArgumentCaptor<Accident> accidentCapture = ArgumentCaptor.forClass(Accident.class);
+        ArgumentCaptor<Set<Integer>> rIdsCapture = ArgumentCaptor.forClass(Set.class);
+
+        verify(accidents).update(accidentCapture.capture(), rIdsCapture.capture());
+
+        assertThat(accidentCapture.getValue()).usingRecursiveComparison()
+                .isEqualTo(accident);
+        assertThat(rIdsCapture.getValue()).usingRecursiveComparison()
+                .isEqualTo(rIds);
+    }
+
+    /**
+     * Проверка метода deleteAccident из AccidentController
+     * @throws Exception
+     */
+    @Test
+    @WithMockUser
+    void whenGetDeleteAccidentTrueThenReturnListPage() throws Exception {
+        this.mockMvc.perform(get("/delete?id=" + anyInt()))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/index"));
+    }
 
     @Test
     @WithMockUser
--- src/test/java/ru/job4j/controller/UserControllerTest.java
+++ src/test/java/ru/job4j/controller/UserControllerTest.java
@@ -4,14 +4,26 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import ru.job4j.Main;
 
+import ru.job4j.model.Authority;
+import ru.job4j.model.User;
+import ru.job4j.repository.springdata.AuthoritySpringDataRepository;
+import ru.job4j.service.springdata.UserSpringDataService;
+
+
+import java.util.Optional;
+
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * 1) @SpringBootTest(classes = Main.class)
@@ -27,9 +39,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 public class UserControllerTest {
+    @MockBean
+    private UserSpringDataService users;
+    @MockBean
+    private AuthoritySpringDataRepository authorities;
     @Autowired
     private MockMvc mockMvc;
 
+    /**
+     * Проверка метода registrationSave из UserController
+     * @throws Exception
+     */
+    @Test
+    @WithMockUser
+    void whenRegistrationSaveThenRedirectLoginPage() throws Exception {
+        var authority = new Authority(1, "ROLE_USER");
+        var user = new User(0, "password", "login", authority, true);
+
+        when(authorities.findByAuthority(authority.getAuthority())).thenReturn(authority);
+        when(users.save(user)).thenReturn(Optional.of(user));
+
+        this.mockMvc.perform(post("/registration")
+                        .param("username", "login")
+                        .param("password", "password"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+    }
+
+    /**
+     * Проверка метода loginPage из UserController
+     * @throws Exception
+     */
     @Test
     public void whenGetLoginPageThenStatusOk() throws Exception {
         this.mockMvc.perform(get("/login"))
@@ -38,12 +79,28 @@ public class UserControllerTest {
                 .andExpect(view().name("login"));
     }
 
+    /**
+     * Проверка метода registrationPage из UserController
+     * @throws Exception
+     */
     @Test
     public void whenGetRegistrationPageThenStatusOk() throws Exception {
         this.mockMvc.perform(get("/registration"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("registration"));
+    }
+
+    /**
+     * Проверка метода registrationPage из UserController
+     * @throws Exception
+     */
+    @Test
+    public void whenlogoutPageThenRedirectLoginLogoutTrue() throws Exception {
+        this.mockMvc.perform(get("/logout"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?logout=true"));
     }
 
 
