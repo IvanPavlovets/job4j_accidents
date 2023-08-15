4c7ff54eeb5155ccff4b95f84ae22bf59b6a4fdd
IvanPavlovets <ivan150287@gmail.com>
2023-08-15 16:06:16 +0700

    1. Spring Liquibase [#302330]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,6 +1,8 @@
 package ru.job4j;
 
 import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import org.springframework.boot.SpringApplication;
@@ -10,12 +12,17 @@ import org.springframework.context.annotation.Bean;
 import javax.sql.DataSource;
 
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
 
     @Bean
     public SpringLiquibase liquibase(DataSource ds) {
         SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setChangeLog("classpath:db/dbchangelog.xml");
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
         liquibase.setDataSource(ds);
         return liquibase;
     }
